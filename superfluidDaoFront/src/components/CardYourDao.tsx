d35b11dbe66efac481fa7035608603a2d750394a
mounia <mouniaarjdal@gmail.com>
2023-09-26 15:16:35 +0200

    add Card Dao componant

--- superfluidDaoFront/src/components/CardYourDao.tsx
+++ superfluidDaoFront/src/components/CardYourDao.tsx
@@ -8,9 +8,18 @@ type Props = {
 const CardYourDao = (props: Props) => {
   return (
     <Link to="/dashboard">
-      <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
-        <h1 className="text-3xl font-bold text-white">{props.name}</h1>
-        <p className="text-gray-300 text-lg mt-3">{props.description}</p>
+      <div className="card card-side bg-base-0 shadow-xs">
+        <div className="bg-gray-800 p-6 rounded-lg shadow-lg flex">
+          <img
+            src="/images/stock/photo-1635805737707-575885ab0820.jpg"
+            alt="Dao1"
+            className="w-24 h-24 object-cover rounded-full mr-4"
+          />
+          <div>
+            <h1 className="text-3xl font-bold text-white text-left">{props.name}</h1>
+            <p className="text-gray-300 text-sm mt-2 text-justify">{props.description}</p>
+          </div>
+        </div>
       </div>
     </Link>
   );
